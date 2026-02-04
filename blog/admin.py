e95f849e0ade75b59d26a08741b04bde6e5a35c2
Pete Blackburn <p.d.blackburn0@gmail.com>
2026-02-04 14:03:14 +0000

    Made migrations for comment app

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Post, Comment
-
 
 # Register your models here.
 admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
-    class Comment(models.Model):
+    
+class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
