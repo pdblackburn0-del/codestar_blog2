d924fd745c9bd1fd00d8a85da86c413aa34dd879
Pete Blackburn <p.d.blackburn0@gmail.com>
2026-02-04 13:58:47 +0000

    Added a comment model to models.py

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+    class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
